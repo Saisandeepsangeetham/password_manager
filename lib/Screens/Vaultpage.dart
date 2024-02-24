f1bc71b03f8a46706999306996e067e7c4761b5c
Saisandeep Sangeetham <134389218+Saisandeepsangeetham@users.noreply.github.com>
2024-02-24 18:43:31 +0530

    password_page

---
+++ lib/Screens/Vaultpage.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class Vaultpage extends StatefulWidget {
+  const Vaultpage({super.key});
+
+  @override
+  State<Vaultpage> createState() => _VaultpageState();
+}
+
+class _VaultpageState extends State<Vaultpage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold();
+  }
+}
--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:project_app/LoginPage.dart';
-import 'package:project_app/register.dart';
+import 'package:project_app/Screens/LoginPage.dart';
+import 'package:project_app/Screens/register.dart';
 
 void  main(){
   runApp(const MaterialApp(
